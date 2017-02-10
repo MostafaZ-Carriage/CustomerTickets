0ad69ded02e1712c4d5fad8f793fb8258bdccbcb
mostafa <mostafa.k.zahran@gmail.com>
2017-02-10 14:48:01 +0300

    add basic structure for user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170210110900) do
+ActiveRecord::Schema.define(version: 20170210114413) do
 
   create_table "tickets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title",                     default: "New Ticket"
     t.text     "description", limit: 65535
     t.datetime "created_at",                                       null: false
     t.datetime "updated_at",                                       null: false
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,36 @@
 require 'test_helper'
 class UserTest < ActiveSupport::TestCase
+  setup do
+    @new_user = User.new
+  end
+
   test 'User class exists' do
     assert class_exists?(User)
+  end
+
+  test 'A user has first name' do
+    assert user_has_attribute?(:first_name)
+  end
+
+  test 'A user has last name' do
+    assert user_has_attribute?(:last_name)
+  end
+
+  test 'A user has email' do
+    assert user_has_attribute?(:email)
+  end
+
+  test 'A user has created_at' do
+    assert user_has_attribute?(:created_at)
+  end
+
+  test 'A user has updated_at' do
+    assert user_has_attribute?(:updated_at)
+  end
+
+  private
+
+  def user_has_attribute?(attribute_name)
+    @new_user.has_attribute?(attribute_name)
   end
 end
